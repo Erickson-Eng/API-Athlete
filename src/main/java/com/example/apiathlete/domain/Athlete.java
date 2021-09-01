5cca0f285ca504c0cef1ca1d3b74b7a9d8b87e9d
Erickson <erickson.tulio96@gmail.com>
2021-09-01 16:19:26 -0300

    created profile child entity

---
+++ src/main/java/com/example/apiathlete/domain/Athlete.java
@@ -0,0 +1,20 @@
+package com.example.apiathlete.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+@Getter
+@Setter
+@RequiredArgsConstructor
+@ToString(onlyExplicitlyIncluded = true)
+public class Athlete extends Profile {
+    private static final long serialVersionUID = -8638214819876514147L;
+    private Double weight;
+    private Double height;
+}
