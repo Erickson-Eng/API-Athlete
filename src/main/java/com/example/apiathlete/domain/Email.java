ab2e12316fa9191f2d6e4d6ad2bc1891058d97e9
Erickson <erickson.tulio96@gmail.com>
2021-09-01 16:16:08 -0300

    association of email and address to profile

--- src/main/java/com/example/apiathlete/domain/Email.java
+++ src/main/java/com/example/apiathlete/domain/Email.java
@@ -20,6 +20,7 @@ public class Email implements Serializable {
     private static final long serialVersionUID = 667011049015452582L;
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(nullable = false)
     private String addressEmail;
 
     @Override
--- src/main/java/com/example/apiathlete/domain/Profile.java
+++ src/main/java/com/example/apiathlete/domain/Profile.java
@@ -4,22 +4,41 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.Hibernate;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Objects;
 
-@Table(name = "profile")
-@Entity
+@MappedSuperclass
 @Getter
 @Setter
 @RequiredArgsConstructor
 @ToString(onlyExplicitlyIncluded = true)
+@Inheritance( strategy = InheritanceType.TABLE_PER_CLASS)
 public class Profile implements Serializable {
     private static final long serialVersionUID = 7663551134425055595L;
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String firstName;
     private String lastName;
+    @JoinColumn(unique = true,nullable = false)
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    private Email email;
+    @JoinColumn(nullable = false)
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    private Address address;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Profile profile = (Profile) o;
+        return Objects.equals(id, profile.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
 }
